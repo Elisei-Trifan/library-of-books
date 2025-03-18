7a40e65139040eb1600464d861ff49898bfeee7a
Elisei-Trifan <slavaberzan@gmail.com>
2025-03-19 00:18:28 +0200

    modal window

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -2,16 +2,56 @@ import React from 'react'
 import Author from '../assets/Author'
 
 const BookCard = ({ book }) => {
-  const { title, author, cover } = book
+  const { title, author, cover, backCover } = book
+  const [isOpen, setIsOpen] = React.useState(false)
+  const [currentPage, setCurrentPage] = React.useState(0)
+
+  const toggleModal = () => setIsOpen(!isOpen)
+
   return (
-    <div className="book_card">
-      <img className="book_card_img" src={cover} alt={title} />
-      <h5 className="book_card_title"> {title}</h5>
-      <div className="book_card_author_cont">
-        <Author width={15} height={15} />
-        <p className="book_card_author">{author}</p>
+    <>
+      <div className="book_card">
+        <img
+          className="book_card_img"
+          src={cover}
+          alt={title}
+          onClick={toggleModal}
+        />
+        <h5 className="book_card_title"> {title}</h5>
+        <div className="book_card_author_cont">
+          <Author width={15} height={15} />
+          <p className="book_card_author">{author}</p>
+        </div>
       </div>
-    </div>
+
+      {/* Модальное окно */}
+      {isOpen && (
+        <div className="modal_overlay" onClick={toggleModal}>
+          <div className="modal_content" onClick={(e) => e.stopPropagation()}>
+            <button className="modal_close" onClick={toggleModal}>
+              &times;
+            </button>
+            <img
+              className="modal_img"
+              src={currentPage === 0 ? cover : backCover}
+              alt={title}
+            />
+            <button
+              className="modal_arrow left"
+              onClick={() => setCurrentPage(0)}
+            >
+              &larr;
+            </button>
+            <button
+              className="modal_arrow right"
+              onClick={() => setCurrentPage(1)}
+            >
+              &rarr;
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 
--- src/data/books.js
+++ src/data/books.js
@@ -4,6 +4,7 @@ export const books = [
     title: 'На краю земли',
     author: 'Владимир Тевс',
     cover: `${process.env.PUBLIC_URL}/images/photo_2025-03-18_16-56-40.jpg`,
+    backCover: `${process.env.PUBLIC_URL}/images/photo_2025-03-18_23-59-33.jpg`,
     category: 'рассказы',
   },
   {
@@ -11,6 +12,7 @@ export const books = [
     title: 'Жизненные бури. Том 1',
     author: 'Владимир Реймер',
     cover: `${process.env.PUBLIC_URL}/images/photo_2025-03-18_16-56-19.jpg`,
+    backCover: '/slider/book_back.jpg',
     category: 'рассказы',
   },
   {
@@ -18,6 +20,7 @@ export const books = [
     title: 'Подвиг веры. Том 2',
     author: 'Владимир Реймер',
     cover: `${process.env.PUBLIC_URL}/images/photo_2025-03-18_16-56-38.jpg`,
+    backCover: '/slider/book_back.jpg',
     category: 'рассказы',
   },
   {
@@ -25,6 +28,7 @@ export const books = [
     title: 'Христианин и его внешность',
     author: 'Генрих Тевс',
     cover: `${process.env.PUBLIC_URL}/images/photo_2025-03-18_16-56-20.jpg`,
+    backCover: '/slider/book_back.jpg',
     category: 'духовно-назидательные',
   },
   {
@@ -32,6 +36,7 @@ export const books = [
     title: 'Молитвенная жизнь Иисуса',
     author: 'Вольфганг Бюне',
     cover: `${process.env.PUBLIC_URL}/images/photo_2025-03-18_16-56-22.jpg`,
+    backCover: '/slider/book_back.jpg',
     category: 'духовно-назидательные',
   },
   {
@@ -39,6 +44,7 @@ export const books = [
     title: 'Жизнь лишь одна',
     author: 'Вера Кушнир',
     cover: `${process.env.PUBLIC_URL}/images/photo_2025-03-18_16-56-23.jpg`,
+    backCover: '/slider/book_back.jpg',
     category: 'рассказы',
   },
   {
@@ -46,6 +52,55 @@ export const books = [
     title: 'Жизнь Мессии',
     author: 'Арнольд Фрухтэнбаум',
     cover: `${process.env.PUBLIC_URL}/images/photo_2025-03-18_16-56-25.jpg`,
+    backCover: '/slider/book_back.jpg',
     category: 'для проповедников',
+  },
+  {
+    id: 8,
+    title: 'Иные же замучены были',
+    author: 'Илья Чебан',
+    cover: `${process.env.PUBLIC_URL}/images/photo_2025-03-18_16-56-28.jpg`,
+    backCover: '/slider/book_back.jpg',
+    category: 'рассказы',
+  },
+  {
+    id: 9,
+    title: 'Григорий и Анна',
+    author: 'Илья Чебан',
+    cover: `${process.env.PUBLIC_URL}/images/photo_2025-03-18_16-56-30.jpg`,
+    backCover: '/slider/book_back.jpg',
+    category: 'рассказы',
+  },
+  {
+    id: 10,
+    title: 'Светильник №27',
+    author: 'Издательство Христианин',
+    cover: `${process.env.PUBLIC_URL}/images/photo_2025-03-18_16-56-31.jpg`,
+    backCover: '/slider/book_back.jpg',
+    category: 'светильник',
+  },
+  {
+    id: 11,
+    title: 'Чистый источник №11',
+    author: 'Издательство Христианин',
+    cover: `${process.env.PUBLIC_URL}/images/photo_2025-03-18_16-56-33.jpg`,
+    backCover: '/slider/book_back.jpg',
+    category: 'чистый источник',
+  },
+  {
+    id: 12,
+    title: 'Вестник Истины №',
+    author: 'Издательство Христианин',
+    cover: `${process.env.PUBLIC_URL}/images/photo_2025-03-18_16-56-34.jpg`,
+    backCover: '/slider/book_back.jpg',
+    category: 'вестник истины',
+  },
+  {
+    id: 13,
+    title: 'Мужество с Иисусом',
+    author: 'Издательство Христианин',
+    cover: `${process.env.PUBLIC_URL}/images/photo_2025-03-18_16-56-36.jpg`,
+    backCover: '/slider/book_back.jpg',
+    category: 'для детей',
   },
 ]
